f27242bf68313144ad327d38de3da62745e3ce10
itsahadul99 <itsahadul999@gmail.com>
2024-02-28 12:47:58 +0600

    added js file and show the phone card

--- js/api.js
+++ js/api.js
@@ -1,1 +1,34 @@
-console.log("Connected");
+const loadData = async (brandName) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+    const data = await res.json()
+    // console.log(data);
+    const phones = data.data;
+    // console.log(phones);
+    displayCard(phones);
+}
+const displayCard = (phones) => {
+    console.log(phones);
+    // get the phone data and show it by card
+    const phoneCardContainer = document.getElementById('card-container');
+    phones.forEach(phone => {
+        // console.log(phone);
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure class="px-10 py-10 pt-10 bg-[#0D6EFD0D]">
+            <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+        </figure>
+            <div class="card-body items-center text-center space-y-5">
+                    <h2 class="card-title">${phone.phone_name}</h2>
+                    <p>There are many variations of passages of available, but the majority have suffered</p>
+                <div class="card-actions">
+                    <button onclick="showDetails('${phone.slug}')" class="btn btn-primary text-xs lg:text-xl">Show Details</button>
+                </div>
+            </div>
+        </div>
+        `
+        phoneCardContainer.appendChild(phoneCard);
+    });
+}
+
+loadData();
